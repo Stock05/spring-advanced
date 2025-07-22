e51a7c20b013077ebe9854c4fd3143dbaf7dad61
Stock05 <qace756@gmail.com>
2025-07-22 11:54:42 +0900

    Refactor: 레벨 1-3: Validation

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,8 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +14,10 @@ public class UserChangePasswordRequest {
 
     @NotBlank
     private String oldPassword;
+
     @NotBlank
+    @Size(min = 8, message = "비밀번호는 반드시 8자 이상이어야 합니다")
+    @Pattern(regexp = ".*\\d.*", message = "비밀번호에는 숫자가 반드시 포함되어야 합니다")
+    @Pattern(regexp = ".*[A-Z].*", message = "비밀번호에는 대문자가 반드시 포함되어야 합니다")
     private String newPassword;
 }
